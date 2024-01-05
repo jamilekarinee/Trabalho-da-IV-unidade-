694256d17f84ed85af65cdf3fc35e230f9f3d514
jamilekarinee <139991955+jamilekarinee@users.noreply.github.com>
2024-01-05 20:15:11 -0300

    backend

---
+++ migrations/V1__criar_tabela_tarefa.sql
@@ -0,0 +1,8 @@
+create table genero (
+ 	id int PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(300) not null
+);
+create table produtora (
+ 	id int PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(300) not null
+); 
